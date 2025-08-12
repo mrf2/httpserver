4cf1ad35a17404c01312f22a96f7d3216fae7bbd
mrf2 <mrf2.github@gmail.com>
2025-08-12 08:08:20 +0600

    update adder.c

--- cgi-bin/adder.c
+++ cgi-bin/adder.c
@@ -2,6 +2,8 @@
  * filename: adder.c
  */
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define	MAXLINE	 8192  /* Max text line length */
 
@@ -22,4 +24,23 @@ int main(void)
        /* char *strcpy(char *restrict dst, const char *restrict src); */
 		strcpy(arg1, buf);
 		strcpy(arg2, ptr+1);
-		
+		n1 = atoi(arg1);
+		n2 = atoi(arg2);
+	}
+
+
+
+	/* Make the response body */
+	sprintf(content, "Welcome to adder.com: ");
+	sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+	sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content,
+			n1, n2, n1 + n2);
+	sprintf(content, "%sThnaks for visiting!\r\n", content);
+
+	/* Generate the HTTP response */
+	printf("Content-length: %d\r\n", (int)strlen(content));
+	printf("Content-type: text/html\r\n\r\n");
+	printf("%s", content);
+	fflush(stdout);
+	exit(0);
+}
